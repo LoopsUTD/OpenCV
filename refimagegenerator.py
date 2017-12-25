e7db8327f62af3312e4dd5ee11fbe4d76280843e
Keene Chin <trix1011@gmail.com>
2017-12-24 19:14:06 -0600

    generate black image in ocvp

--- refimagegenerator.py
+++ refimagegenerator.py
@@ -4,4 +4,11 @@
 """
 import numpy as np
 import cv2
+import matplotlib as mat
+
+img=np.zeros((200,200))
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
