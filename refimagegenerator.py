00bd2cf8ff516118c04da52f096bea666563d71c
Keene Chin <trix1011@gmail.com>
2017-12-24 20:14:30 -0600

    generating green stripes

--- refimagegenerator.py
+++ refimagegenerator.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python
 
 """
+Script to generate reference images for spatial deviation analysis
 """
 import numpy as np
 import cv2
 import matplotlib as mat
+"""
+Image properties
+"""
+rsize=600
+csize=400
+period=6.0
+dutycycle=0.5
+cutoff=period*(dutycycle)
+colorvect=np.array([[0,255,0]])
+img=np.zeros((rsize,csize,3))
+"""
+Stripe addition
+"""
+k=0
+for i in range(1,rsize):
+    k=k+1
+    if k<=cutoff:
+        img[i,:,:]=np.dot(np.ones((csize,1)),colorvect)
+    if k==period:
+        k=0
 
-img=np.zeros((200,200))
+
+"""
+Display generated image
+
+"""
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 cv2.imshow('image',img)
 cv2.waitKey(0)
