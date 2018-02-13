6171df6d1929d4edc0de19fd36bee61dbeb2a833
Kaeladin <trix1011@gmail.com>
2018-02-13 14:07:09 -0600

    Added classes and data for moving deviation data around

--- deviationmap.py
+++ deviationmap.py
@@ -3,7 +3,7 @@ import blob
 
 
 def main():
-	map = [[blob.Blob(j*i+j, j, i) for i in range(601)] for j in range(401)]
+	map = [[blob.Blob(j*i+j, j, i) for i in range(600)] for j in range(400)]
 	for rows in map:
 		for blobs in rows:
 			print(str(blobs.id)+" "+str(blobs.value))
