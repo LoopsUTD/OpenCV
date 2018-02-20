bbd0be7ea3a9d5c6c2b3a956c5543e54c9ec9807
Keene Chin <trix1011@gmail.com>
2018-02-20 21:36:32 +0000

    1 Rotation for stepper motor

---
+++ stepTest.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+def setpins():
+    stepPin = 29
+    dirPin = 31
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(stepPin,GPIO.OUT)
+    GPIO.setup(dirPin,GPIO.OUT)
+    
+def step(steps,stepPin,dirPin):
+    stepsTaken = 0
+    GPIO.output(dirPin,GPIO.HIGH)
+    if steps<0:
+        GPIO.output(dirPin,GPIO.LOW)
+    if steps>0:
+        GPIO.output(dirPin,GPIO.HIGH)
+    while stepsTaken<abs(steps):
+        GPIO.output(stepPin,GPIO.HIGH)
+        time.sleep(.0000050)
+        GPIO.output(stepPin,GPIO.LOW)
+        time.sleep(.00005)
+        stepsTaken = stepsTaken+1
+    GPIO.output(stepPin,GPIO.LOW)
+    
+if __name__ == "__main__":
+    setpins()
+    step(1600,29,31)
+    step(-1600,29,31)
+    time.sleep(1)
+    GPIO.cleanup()
