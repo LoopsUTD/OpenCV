38c235d7cdf94255c996ec8237ab99aeebefc0fa
Keene Chin <trix1011@gmail.com>
2018-02-21 08:59:28 +0600

    Ability to set home and return to home for linact@

--- dummyControl.py
+++ dummyControl.py
@@ -7,11 +7,19 @@ import threading
 def on_press(key):
     try:
         global stop
+        global goal
+        global home
+        global current
         if key.char=='w':
             stop=-10
+            
             return False
         if key.char=='s':
             stop=10
+            return False
+        if key.char=='x':
+            stop=11
+            home=current
             return False
     except AttributeError:
         if key==keyboard.Key.down:
@@ -20,21 +28,23 @@ def on_press(key):
         if key==keyboard.Key.up:
             stop=-1
             return False
-        print('special key {0} pressed'.format(key))
+        if key == keyboard.Key.space:
+            goal=home
+            stop=11
+            return False
+        
 
 def on_release(key):
-    try:
+        global current
+        global home
         global stop
         if key == keyboard.Key.esc:                                             
             stop=99
             return False
-    except AttributeError:
-        print('special key {0} pressed'.format(key))
 
 
 def moveTo(target):
     global current
-    current=0
     motor.step(target-current)
     current=target
 
@@ -43,10 +53,16 @@ if __name__ == '__main__':
     mode=int(argv[1])
     print ('{}{}'.format('mode: ',mode))
     motor.setpins(29,31)
+    global current
+    global home
+    global goal
+    home=0
+    current=home
+    goal=current
     if (mode==0):
-        desiredpos=int(argv[2])
-        print('{}{}'.format("Stepping to ",desiredpos))
-        motor.step(desiredpos) 
+        goal=int(argv[2])
+        print('{}{}'.format("Stepping to ",goal))
+        moveTo(goal) 
     if (mode==1):
         global stop
         adjust=True
@@ -59,6 +75,8 @@ if __name__ == '__main__':
             if(stop==99):
                 adjust=False
             if(abs(stop)<=10):
-                motor.step(stop*100)
+                goal=current+stop*100
+            print(goal)
+            moveTo(goal)
             
     motor.GPIO.cleanup()
