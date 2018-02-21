984398a66333ae7e2812fb22d54ecb271835d431
Keene Chin <trix1011@gmail.com>
2018-02-21 07:49:38 +0600

    added second mode, figuring out keyboard interrupt

--- dummyControl.py
+++ dummyControl.py
@@ -1,10 +1,48 @@
 import step as motor
 import sys
-def moveTo(pos):
-    step.step(pos,29,31)
+from pynput import keyboard
+from multiprocessing import Queue
+
+
+def on_press(key):
+    global stop
+    stop=1
+    try:
+        stop=1
+        print('alphanumeric key {0} pressed'.format(
+                                    key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(key))
+
+def on_release(key):
+    print('{0} released'.format( 
+         key))
+    if key == keyboard.Key.esc:                                                      # Stop listener 
+        return False
+# Collect events until released
+
+
 
 if __name__ == '__main__':
     from sys import argv
-    motor.setpins()
-    motor.step(int(argv[1]),29,31) 
+    mode=int(argv[1])
+    desiredpos=int(argv[2])
+    print ('{}{}'.format('mode: ',mode))
+    motor.setpins(29,31)
+    if (mode==0):
+        print('{}{}'.format("Stepping to ",desiredpos))
+        motor.step(desiredpos) 
+    if (mode==1):
+
+         
+        while(True):
+            global stop
+            stop=0
+            with keyboard.Listener(
+                on_press=on_press,
+                on_release=on_release) as listener:
+                    listener.join()
+            print(stop)
+            if(stop!=1):
+                motor.step(-100)
     motor.GPIO.cleanup()
