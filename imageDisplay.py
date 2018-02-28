f9acdc5909afcb7fb727f571cf8ed32508c02c15
araei3 <33303026+araei3@users.noreply.github.com>
2018-02-28 15:22:34 -0600

    Removing Extra

--- imageDisplay.py
+++ imageDisplay.py
@@ -1,1 +1,21 @@
-infbkdsnjskdf 
+import numpy as np
+import cv2
+
+def display(imagename, x, y):
+	cv2.namedWindow("output", cv2.WINDOW_NORMAL)        
+	im = cv2.imread(imagename)													
+	cv2.imshow("output", im) 
+	height = im.shape[0]
+	width = im.shape[1]
+	print(im.shape)
+	cv2.resizeWindow("output", (1920, 1080))
+	cv2.moveWindow("output", x,y)
+	cv2.waitKey(0)
+
+if __name__ == '__main__':
+	imagename = "dsc03113.jpg"
+	x = 1920
+	y = -5
+	display(imagename,x,y)
+	
+	#1920, -5
