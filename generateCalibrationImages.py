bbeea4e14fbc2d3fc2239b9d5e0d30fc1e545b0c
DavidMerzJr <dmmjr2@gmail.com>
2018-02-28 18:20:09 -0600

    Display & Calib. Cleanup
    Removed some unnecessary imports, fixed some typos, squashed some bugs

--- generateCalibrationImages.py
+++ generateCalibrationImages.py
@@ -1,31 +1,36 @@
 import tkinter
 import numpy
 import cv2
+import imageDisplay
 
 # INPUT: 'calib.png', the name of the checkerboard/calibration image, as a string
 # OUTPUT: filenames, array containing names of the 16 images captured
 # FILE I/O: saves 16 images
 
-def generateCalibrationImages():
+def generateCalibrationImages(refImage):
     rootWindow   = tkinter.Tk()
-    screenWidth  = rootWindow.winfo_screenWidth()
-    screenHeight = rootWindow.winfo_screenHeight()
+    screenWidth  = rootWindow.winfo_screenwidth()
+    screenHeight = rootWindow.winfo_screenheight()
 
-    board = cv2.imread('calib.png')
-    boardHeight, boardWidth, boardChannels = board.shape
+    board = cv2.imread(refImage)
+    boardHeight, boardWidth = board.shape[:2]
 
     positions = numpy.zeros((16,2))
     filenames = [16]
 
-    for i in range(0,3):
-        for j in range(0,3):
-            positions[4*i+j,0] = i * (screenWidth - boardWidth)/3
-            positions[4*i+j,1] = j * (screenHeight - boardHeight)/3
+    for i in range(0,4):
+        for j in range(0,4):
+            positions[4*i+j,0] = i * (screenHeight - boardHeight)/3
+            positions[4*i+j,1] = j * (screenWidth - boardWidth)/3
+
+    print(positions)
 
     for i in range(0,16):
-        #displayImage(board, positions[i,0], positions[i,1]
-        #captureImage
-        #saveImage, grab filename
-        filenames[i] = filename
+        imageDisplay.display(refImage, int(positions[i,1]), int(positions[i,0]))
+        #filename = captureImage()
+        #filenames[i] = filename
 
     return filenames
+
+if __name__ == '__main__':
+    print(generateCalibrationImages("dragon_turtle.jpg"))
--- imageDisplay.py
+++ imageDisplay.py
@@ -1,4 +1,3 @@
-import numpy as np
 import cv2
 
 def display(imagename, x, y):
@@ -7,9 +6,8 @@ def display(imagename, x, y):
 	cv2.imshow("output", im) 
 	height = im.shape[0]
 	width = im.shape[1]
-	print(im.shape)
-	cv2.resizeWindow("output", (1920, 1080))
-	cv2.moveWindow("output", x,y)
+	cv2.resizeWindow("output", (width, height))	#ensures the image will be displayed at precisely 100% scale
+	cv2.moveWindow("output", x,y)				#places the corner of the display window at x,y - unfortunately meaning the image is offset
 	cv2.waitKey(0)
 
 if __name__ == '__main__':
