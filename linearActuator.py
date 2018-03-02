8ffc2177f6c3fddaa9d1902955751e1134f3217b
Keene Chin <trix1011@gmail.com>
2018-03-02 15:43:57 -0600

    testing tkinter event binding

--- linearActuator.py
+++ linearActuator.py
@@ -10,6 +10,17 @@ class LinearActuator:
 
 if __name__ == "__main__":
     root =  Tk()
-    w = Label(root, text="Hello, world!")
-    w.pack()
+    def leftKey(event):
+        print("Left key pressed")
+     
+    def rightKey(event):
+        print("Right key pressed")
+             
+    frame = Frame(root, width=100, height=100)
+    frame.bind('<Left>', leftKey)
+    frame.bind('<Right>', rightKey)
+    frame.focus_set()
+    frame.pack()
     root.mainloop()
+
+
