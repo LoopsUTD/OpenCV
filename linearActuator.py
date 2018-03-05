09da378b8dffc2642b13f270725e605406fec2c0
Keene Chin <trix1011@gmail.com>
2018-03-04 18:21:57 -0600

    Two speeds of step, close button exits everything

--- linearActuator.py
+++ linearActuator.py
@@ -3,12 +3,17 @@ import sys
 from tkinter import *
 
 class LinearActuator:
+    #stepperMotor class from stepperMotor.py
+    #home - adjustable position to return to (in steps)
+    #current - current position (in steps_
+    #goal - position that actuator is moving towards
     def __init__(self):
         self.motor=stepperMotor.StepperMotor(29,31,5,3)
         self.home=0
         self.current=self.home
         self.goal=self.home
     
+    #Returns boolean statement corresponding to whether goal was reached
     def moveTo(self,target):
         success=self.motor.step(target-self.current)
         self.current=target
@@ -29,25 +34,33 @@ class LinearActuator:
         self.moveTo(self.home)
     
         print('{}{}{}{}'.format('Bottom: ', self.bottom,'\nTop: ', self.top))
+    
+    def moveUp(self,event,step):
+        self.moveTo(self.current+step)
 
+    def moveDown(self,event,step):
+        self.moveTo(self.current-step)
+
+    def setHome(self,event):
+        self.home=self.current
+    
+    def goHome(self,event):
+        self.moveTo(self.home)
     
     def manualAdjust(self,stepSize):
         root = Tk()
-        def moveUp(event):
-            self.moveTo(self.current+stepSize)
-        def moveDown(event):
-            self.moveTo(self.current-stepSize)
-        def setHome(event):
-            self.home=self.current
-        
         frame = Frame(root,width=100,height=100)
-        label = Label(root,text='Move with arrow keys, Press space to set home.')
+        label = Label(root,text='Move with arrow keys, Press x to set home. Press space to got home.')
         label.pack()
-        frame.bind('<Up>', moveUp)
-        frame.bind('<Down>', moveDown)
-        frame.bind('<space>', setHome)
+        frame.bind('<Up>', lambda event: self.moveUp(event,stepSize/10)  )
+        frame.bind('<Down>', lambda event: self.moveDown(event,stepSize/10))
+        frame.bind('<w>', lambda event: self.moveUp(event,stepSize)  )
+        frame.bind('<s>', lambda event: self.moveDown(event,stepSize))
+        frame.bind('<x>', self.setHome)
+        frame.bind('<space>',self.goHome)
         frame.focus_set()
         frame.pack()
+        Button(root, text="Quit", command=quit).pack()
         root.mainloop()
     
     def moveIntoPath(self):
@@ -60,6 +73,6 @@ if __name__ == "__main__":
     actuator=LinearActuator()
     actuator.findLimits()
     actuator.manualAdjust(stepSize=100)
-    actuator.moveIntoPath()
+    #actuator.moveIntoPath()
     actuator.moveOutOfPath()
     actuator.moveIntoPath()
