0de1728b0c7e3dde948d3fe0674d0ec7ef2e3400
Merz <dmm140330@utdallas.edu>
2018-03-05 13:18:07 -0600

    Removing Debugging Statements
    Commented out debugging print() calls, removed a possibly extraneous __future__ import statement.

--- capture_raw.py
+++ capture_raw.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 import logging
 import os
 import subprocess
@@ -28,22 +26,22 @@ def main():
     gp.check_result(gp.gp_camera_set_config(camera, config))    
     endConfig = time.time()*1000
     
-    print('Capturing image')
+    #print('Capturing image')
     imgTimeStart = time.time()*1000
     file_path = gp.check_result(gp.gp_camera_capture(camera, gp.GP_CAPTURE_IMAGE))
-    print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
+    #print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
     target = os.path.join('RAW/', file_path.name)
     endImgCap = time.time()*1000
-    print('Copying image to', target)
+    #print('Copying image to', target)
     saveImgStart = time.time()*1000
     camera_file = gp.check_result(gp.gp_camera_file_get(camera, file_path.folder, file_path.name, gp.GP_FILE_TYPE_RAW))
     gp.check_result(gp.gp_file_save(camera_file, target))
     saveImgEnd= time.time()*1000
     subprocess.call(['xdg-open', target])
     gp.check_result(gp.gp_camera_exit(camera))
-    print("Config Time: %f" % (endConfig - configStartTime))
-    print("Img Capture Time: %f" % (endImgCap - imgTimeStart))
-    print("Img Save Time: %f" % (saveImgEnd - saveImgStart))
+    #print("Config Time: %f" % (endConfig - configStartTime))
+    #print("Img Capture Time: %f" % (endImgCap - imgTimeStart))
+    #print("Img Save Time: %f" % (saveImgEnd - saveImgStart))
     return 0
 
 if __name__ == "__main__":
