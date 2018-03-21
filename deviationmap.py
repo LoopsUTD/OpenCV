a24d3cee483b70ed9fc71530340498fbc51d916f
Kaeladin <trix1011@gmail.com>
2018-03-21 16:13:48 -0500

    deviationmap updated with __init__

--- deviationmap.py
+++ deviationmap.py
@@ -1,7 +1,13 @@
-import numpy
+import numpy as np
 import blob
 
 
+class deviationMap:
+	def __init__(self,original,deviated):
+		self.deviation=difference(original,deviation)
+
+	def difference(original, deviated):
+		return deviated-original
 def main():
 	map = [[blob.Blob(j*i+j, j, i) for i in range(600)] for j in range(400)]
 	for rows in map:
