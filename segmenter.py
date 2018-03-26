bb6e573d445bdfe2aa7f83ac4965294b6a317307
David <dmmjr2@gmail.com>
2018-03-26 14:01:39 -0500

    Changed __main__ case on segmenter.py
    __main__ on segmenter.py now runs loud version, displaying progress as we go along

--- segmenter.py
+++ segmenter.py
@@ -132,4 +132,4 @@ def segmentInfo(img):
 
 if __name__ == '__main__':
     # this image name is not important.  I was just using what I had on my computer
-    extractObjects('segmentTest.png')   
+    loudExtractObjects('segmentTest.png')   
