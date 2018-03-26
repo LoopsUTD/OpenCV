ee7b5b9ba0f6a6152efdb2edfec316b81e3825af
Kaeladin <trix1011@gmail.com>
2018-03-26 17:52:01 -0500

    move testData into own file, functionalized correlate

--- correlate.py
+++ correlate.py
@@ -1,49 +1,42 @@
 import cv2
-import math
 from blob import *
 import numpy as np
-from random import *
 from math import *
 import os.path
 from time import *
+from testData import *
 def main(undeviated,deviated):
-	print("main")
-	undevdict={}
-	devdict={}
-	devmap=np.array([])
 
 	fname="dev.txt"
-	if(os.path.isfile(fname)):
-		os.remove(fname)
+#	if(os.path.isfile(fname)):
+#		os.remove(fname)
+	undevdict,devdict=corr(undeviated,deviated)
+	devmap=compare(undevdict,devdict)
+	writeToFile(devmap,fname)
+	
+def compare(undevdict,devdict):
+	devmap={}
+	for key,value in undevdict.items():
+		dist=distance(value,devdict[key])
+		deviation={(value.value[0],value.value[1]):dist}
+		devmap.update(deviation)
+	return devmap
+
+def corr(undeviated,deviated):
+	undevdict={}
+	devdict={}
 	for i in range(undeviated.size):
 		undevdict.update({undeviated[i].id:undeviated[i]})
 		closest=mindist(undeviated[i],deviated)
 		devdict.update({closest.id:closest})
-	for key,value in undevdict.items():
-		dist=distance(value,devdict[key])
-		deviation={dist:value.value}
-		np.append(devmap,deviation)
-		writeToFile(deviation,fname)
+	return undevdict,devdict
 
 def writeToFile(mapping,name):
-	file=open(name,"a")
+	file=open(name,"w")
 	for value in mapping.items():
-		file.write("{},{},{}\n".format(value[1][0],value[1][1],value[0]))
+		file.write("{},{},{}\n".format(value[0][0],value[0][1],value[1]))
 	file.close()
 	
-def testData(size,density):
-	print("testData")
-	undev=np.empty(density,dtype=Blob)
-	dev=np.empty(density,dtype=Blob)
-	for i in range(density):
-		x=randint(0,size)
-		y=randint(0,size)
-		b=Blob(i,x,y)
-		c=Blob(i+randint(0,density),x+randint(density/-20,density/20),y+randint(density/-20,density/20))
-		undev.put(i,b)
-		dev.put(i,c)
-	return undev,dev
-
 def mindist(item,array):
 	minim=sys.maxsize
 	for i in range(array.size):
@@ -55,10 +48,10 @@ def mindist(item,array):
 	return closest
 	
 def distance(blob1,blob2):
-	dist=sqrt(math.pow((blob1.value[0]-blob2.value[0]),2)+math.pow((blob1.value[1]-blob2.value[1]),2))
+	dist=sqrt(pow((blob1.value[0]-blob2.value[0]),2)+pow((blob1.value[1]-blob2.value[1]),2))
 	return dist
 if __name__=='__main__':
-	print("isit")
+	print("main")
 	start = time()
 	data=testData(5000,3000)
 	main(data[0],data[1])
--- displayDeviationMap.m
+++ displayDeviationMap.m
@@ -1,1 +1,3 @@
 scatter(dev.VarName1,dev.VarName2,[],dev.VarName3,'filled')
+figure
+histogram(table2array(dev(:,3)))
