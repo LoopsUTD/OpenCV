ac2832cb05e96e4b2ae0f854b4ead7ee7bdfbbe2
Kaeladin <trix1011@gmail.com>
2018-03-27 19:49:30 -0500

    intermediate timers and title

--- displayDeviationMap.m
+++ displayDeviationMap.m
@@ -2,6 +2,7 @@ dat=table2array(dev)
 dat=dat(dat(:,3)<5,:)
 %%dat(:,3)=~isoutlier(dat(:,3)).*dat(:,3);
 scatter(dat(:,1),dat(:,2),[],dat(:,3),'filled')
+title("Lens 2 Period 4 Single Pixel 3-23")
 colorbar
 figure
 histogram(dat(:,3))
--- segandcorr.py
+++ segandcorr.py
@@ -3,19 +3,21 @@ import correlate
 from time import *
 if __name__=="__main__":	
 	start=time()
-	undevname="lens2_nolens_4pxG.png"
-	devname="lens2_wlens_4pxG.png"
-#j	undevname="undevdumb.png"
-#	devname="devdumb.png"
+#	undevname="lens2_nolens_4pxG.png"
+#	devname="lens2_wlens_4pxG.png"
+	undevname="undevdumb.png"
+	devname="devdumb.png"
 	undev=segmenter.extractObjectsPngJpg(undevname)
-	print('undev done')
+	print('undev segmented')
+	print(time()-start)
 #	for blobs in undev:
 #		print("u")
 #		print(blobs)
 	dev=segmenter.extractObjectsPngJpg(devname)
-	print('dev done')
+	print('dev segmented')
+	print(time()-start)
 #	for blobs in dev:
 #		print("d")
 #		print(blobs)
 	correlate.main(undev,dev)
-	print (time()-start)
+	print ('Total time elapsed: {} seconds'.format(time()-start))
