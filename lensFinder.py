ee4547a7b9920fcb2447e2e9e638b8ec089d0b18
David <dmmjr2@gmail.com>
2018-04-16 15:02:18 -0500

    Messing with LensFinder
    Trying to detect edges better.  Works for minimal tint, can't see clear.

--- lensFinder.py
+++ lensFinder.py
@@ -7,25 +7,26 @@ def findLens(filename):
 	
 	img = cv2.resize(img, (width//4,height//4))
 	
-	img = cv2.medianBlur(img,9)
+	img = cv2.medianBlur(img,3)
 	print('blur done')
 	cv2.imshow('blur done',img)
 	cv2.waitKey(0)
-	ret, img = cv2.threshold(img,100,255,cv2.THRESH_BINARY)
+	ret, img = cv2.threshold(img,64,255,cv2.THRESH_BINARY)
 	print('thresh done')
 	cv2.imshow('thresh done',img)
 	cv2.waitKey(0)
 	
-#	cv2.imshow('edges',cv2.Canny(img,100,200))
-#	cv2.waitKey(0)
+	cv2.imshow('edges',cv2.Canny(img,100,200))
+	cv2.waitKey(0)
 	
-	circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT,1,img.shape[0]/16,param1=100,param2=30,minRadius=1800,maxRadius=2200)
+	circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+#	circles = np.uint16(np.around(circles))
 	
 	cv2.destroyAllWindows()
 	
 	return circles
 
 if __name__ == '__main__':
-	circles = findLens('DSC03070.JPG')
+	circles = findLens('withLens_DSC_0279.JPG')
 	print(circles)
 	print(np.shape(circles))
