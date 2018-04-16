f920bcc773b5773d69854137fc780e9947451e1e
DavidMerzJr <dmmjr2@gmail.com>
2018-04-16 12:34:11 -0500

    Messing with lensFinding filtering
    Working on pre-processing to optimize lens detection

--- lensFinder.py
+++ lensFinder.py
@@ -3,11 +3,25 @@ import numpy as np
 
 def findLens(filename):
 	img = cv2.cvtColor(cv2.imread(filename),cv2.COLOR_BGR2GRAY)
-	img = cv2.medianBlur(img,5)
-	print('blur done')
+	height, width = img.shape
 	
-	circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=1900,maxRadius=2100)
-	circles = np.uint16(np.around(circles))
+	img = cv2.resize(img, (width//4,height//4))
+	
+	img = cv2.medianBlur(img,9)
+	print('blur done')
+	cv2.imshow('blur done',img)
+	cv2.waitKey(0)
+	ret, img = cv2.threshold(img,100,255,cv2.THRESH_BINARY)
+	print('thresh done')
+	cv2.imshow('thresh done',img)
+	cv2.waitKey(0)
+	
+#	cv2.imshow('edges',cv2.Canny(img,100,200))
+#	cv2.waitKey(0)
+	
+	circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT,1,img.shape[0]/16,param1=100,param2=30,minRadius=1800,maxRadius=2200)
+	
+	cv2.destroyAllWindows()
 	
 	return circles
 
