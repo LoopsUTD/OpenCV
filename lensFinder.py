65e660ad9b90bc001ff95242f8a31d15fcc9ec91
David <dmmjr2@gmail.com>
2018-04-18 16:27:42 -0500

    Hopefully Functional Lens Detection
    Using ~contours~ this time.  Scans for the most circular contour, then bounds it in a circle and returns that circle.

--- lensFinder.py
+++ lensFinder.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import math
 
 def findLens(filename, scaling = None):
 	if scaling == None:
@@ -24,27 +25,43 @@ def findLens(filename, scaling = None):
 	print('close done')
 	cv2.imshow('close done',img)
 	cv2.waitKey(0)	
-	cv2.imshow('edges',cv2.Canny(img,100,200))
-	cv2.waitKey(0)
-
+# 	cv2.imshow('edges',cv2.Canny(img,100,200))
+# 	cv2.waitKey(0)
+	im2, contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	circle = None
+	circlePiError = 100
+	for c in contours:
+		cPi = (cv2.arcLength(c,True)**2) / (4*cv2.contourArea(c))
+		cPiError = (cPi-math.pi)**2
+		if cPiError < circlePiError:
+			circle = c
+			circlePiError = cPiError
+			
+	(x,y),r = cv2.minEnclosingCircle(circle)
+	
+	return (x,y,r)
 #	circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,100)	
-	circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,30,param1=30,param2=15,minRadius=0,maxRadius=height//2)
-	if circles.dtype != None:
-		circles = np.uint16(np.around(circles))	
-		circles = circles * scaling
+# 	circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,30,param1=30,param2=15,minRadius=0,maxRadius=height//2)
+# 	if circles.dtype != None:
+# 		circles = np.uint16(np.around(circles))	
+# 		circles = circles * scaling
 	
 	cv2.destroyAllWindows()
 	
-	return circles
+	return contours
 
 if __name__ == '__main__':
-	circles = findLens('withLens_DSC_0284.JPG')
-	print(circles)
-	print(np.shape(circles))
+	circle = findLens('withLens_DSC_0284.JPG')
+# 	print(circles)
+# 	for contour in circles:
+# 		print(cv2.isContourConvex(contour))
+# 	print(np.shape(circles))
 	image = cv2.imread('withLens_DSC_0284.JPG')
 #	image = np.zeros(image.shape)
-	for i in circles[0,:]:
-		cv2.circle(image,(i[0],i[1]),i[2],(0,255,0),2)
+# 	for i in circles[0,:]:
+# 		cv2.circle(image,(i[0],i[1]),i[2],(0,255,0),2)
+# 	cv2.drawContours(image,circles,-1,(0,255,0),3)
+	image = cv2.circle(image,(circle[0],circle[1]),circle[2],(0,0,255),3)
 	cv2.imshow('circles',image)
 	cv2.imwrite('foundCircle.png',image)
 	cv2.waitKey(0)
