7e35c828ef618dc371b22de7c7ff14594ad1943c
DavidMerzJr <dmmjr2@gmail.com>
2018-04-19 00:42:27 -0500

    Cleanup on cropper.py
    Removed some commented-out code, some unnecessary print lines, etc.

--- cropper.py
+++ cropper.py
@@ -1,5 +1,5 @@
 import numpy as np
-import cv2			# only used when ran as main
+import cv2
 import lensFinder	# only used when ran as main
 
 # Function cropImage()
@@ -15,26 +15,16 @@ import lensFinder	# only used when ran as main
 
 def cropToCircle(img, circle):
 	(x,y,r) = circle
-	rsize = img.shape[0]	# These can be used to error-check the bounds of the copying iteration
-	csize = img.shape[1]	# However, the lens should always fall entirely within the image, so it's *probably* irrelevant
 	newImg = np.zeros(img.shape)
 	newImg = cv2.circle(newImg,(x,y),r,(1,1,1),-1)
 	newImg[:,:,0] = newImg[:,:,0] * img[:,:,0]
 	newImg[:,:,1] = newImg[:,:,1] * img[:,:,1]
 	newImg[:,:,2] = newImg[:,:,2] * img[:,:,2]
-# 	maxDist = r**2
-# 	for i in range(y-r,y+r):
-# 		for j in range(x-r,x+r):
-# 			if (i-circle[0])**2 + (j-circle[1])**2 >= maxDist:
-# 				newImg[i,j] = img[i,j]
 	return newImg
 
 if __name__ == '__main__':
 	image = cv2.imread('withLens_DSC_0284.JPG')
-	print('image read')
 	circle = lensFinder.findLens(image.copy())
-	print('lens found')
-	print(circle)
 	image = cropToCircle(image,circle)
 	cv2.imshow('cropped',image)
 	cv2.waitKey(0)
