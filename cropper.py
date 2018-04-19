5980009238caeb833d5617a7362b6da7ece5cad4
DavidMerzJr <dmmjr2@gmail.com>
2018-04-19 00:40:45 -0500

    Exponentially Faster Cropping
    Using ~Matrix Multiplication~ - How fancy!

--- cropper.py
+++ cropper.py
@@ -7,7 +7,7 @@ import lensFinder	# only used when ran as main
 #  the function eliminates all image features
 #  outside the circle.
 # INPUTS:
-#  img	- 2-D array (1 channel) bitmap of image
+#  img	- 2-D array (3 channel) bitmap of image
 #  circle - 3-tuple, (x-center, y-center, radius)
 # OUTPUTS:
 #  newImg	- black image, with everything inside
@@ -18,11 +18,15 @@ def cropToCircle(img, circle):
 	rsize = img.shape[0]	# These can be used to error-check the bounds of the copying iteration
 	csize = img.shape[1]	# However, the lens should always fall entirely within the image, so it's *probably* irrelevant
 	newImg = np.zeros(img.shape)
-	maxDist = r**2
-	for i in range(x-r,x+r):
-		for j in range(y-r,y+r):
-			if (i-circle[0])**2 + (j-circle[1])**2 >= maxDist:
-				newImg[i,j] = img[i,j]
+	newImg = cv2.circle(newImg,(x,y),r,(1,1,1),-1)
+	newImg[:,:,0] = newImg[:,:,0] * img[:,:,0]
+	newImg[:,:,1] = newImg[:,:,1] * img[:,:,1]
+	newImg[:,:,2] = newImg[:,:,2] * img[:,:,2]
+# 	maxDist = r**2
+# 	for i in range(y-r,y+r):
+# 		for j in range(x-r,x+r):
+# 			if (i-circle[0])**2 + (j-circle[1])**2 >= maxDist:
+# 				newImg[i,j] = img[i,j]
 	return newImg
 
 if __name__ == '__main__':
@@ -32,6 +36,7 @@ if __name__ == '__main__':
 	print('lens found')
 	print(circle)
 	image = cropToCircle(image,circle)
-	cv2.imshow(image)
+	cv2.imshow('cropped',image)
 	cv2.waitKey(0)
+	cv2.imwrite('cropped.png',image)
 	cv2.destroyAllWindows()
