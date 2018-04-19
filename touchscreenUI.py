044bd1052231f85dd1b21b4efcc4ab99b73509c3
thedhruvn <11063206+thedhruvn@users.noreply.github.com>
2018-04-18 23:38:30 -0500

    removed super() of app

--- touchscreenUI.py
+++ touchscreenUI.py
@@ -263,7 +263,8 @@ def main_process(shared_data_dict, is_master, exit_event):
 
 	class LoopsApp(App):
 		def __init__(self):
-			super(LoopsApp, self).__init__()
+			App.__init__()
+			#super(LoopsApp, self).__init__()
 
 			if is_master:
 				self.title = "Master Display"
