517caddbb45603e51e3d9b10695bb66423befa75
David <dmmjr2@gmail.com>
2018-04-25 14:11:29 -0500

    bug fix in calculatePower

--- globalPower.py
+++ globalPower.py
@@ -12,11 +12,11 @@ import numpy as np
 #  power - floating-point number representing magnifying power of lens
 
 def calculatePower(noLens,wLens):
-	original = lf.findLens(noLens)
-	magnified = lf.findLens(wLens)
+	original = lf.findLens(noLens)	# returns (x,y,r) of circle
+	magnified = lf.findLens(wLens)	# returns (x,y,r) of circle
 	distLensToCamera = 0.3			# In meters, accurate within 0.03
-	magnification = magnified/original
-	power = (1 - magnified/original)/distLensToCamera
+	magnification = magnified[2]/original[2]
+	power = (1 - magnified[2]/original[2])/distLensToCamera
 	return magnification, power
 
 # Function demagnify()
