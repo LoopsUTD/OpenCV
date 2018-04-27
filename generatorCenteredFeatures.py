5c1796fb31dedda17dd31c6c25bdd9b8d3dc8895
David <dmmjr2@gmail.com>
2018-04-26 21:31:04 -0500

    Refactoring generatorCenteredFeatures
    - Marked significance of seemingly unusued numpy import
    - Removed fourCorners() and its associated comments and demonstration code
    - moved cv2 import to demonstration code, as that is the only location it is used
    - commented out demonstration code

--- generatorCenteredFeatures.py
+++ generatorCenteredFeatures.py
@@ -1,5 +1,4 @@
-import cv2
-import numpy as np
+import numpy as np	# Useful because incoming images are numpy arrays
 
 # A NOTE ON NOTATION:
 # The way numpy numbers its arrays is
@@ -49,48 +48,13 @@ def filledCircle(x,y,r,img):
 				img[i,j] = 255
 	return img
 
-# - - - - - - - - - - - #
-# Function fourCorners
-# - - - - - - - - - - - #
-#   Draws a 2x2 checkerboard
-#   in an image with center
-#   at a specified location
-# Input:
-#   x - x position of center
-#   y - y position of center
-#   img - 8-bit integer matrix
-# Output:
-#   modified version of img,
-#   with 2x2 grid drawn.
-# NOTE:
-#   matrices, like img, are
-#   passed to functions by
-#   reference, which means img
-#   is automatically modified
-#   in the calling function
-# - - - - - - - - - - - #
-
-def fourCorners(x,y,img):
-	rsize = img.shape[0]
-	csize = img.shape[1]
-	for i in range(0,rsize):
-		for j in range(0,csize):
-			if i < x:
-				if j > y:
-					img[i,j] = 255
-			else:
-				if j < y:
-					img[i,j] = 255
-	return img
-
+"""
 if __name__ == '__main__':
+	import cv2
 	image = np.zeros([900,1920,3])
 	filledCircle(image.shape[0]//2,image.shape[1]//2,100,image[:,:,1])
 	cv2.imshow('circleTest',image)
 	cv2.waitKey(0)
 	cv2.imwrite('circle.png',image)
-	fourCorners(image.shape[0]//2,image.shape[1]//2,image[:,:,1])
-	cv2.imshow('cornersTest',image)
-	cv2.imwrite('centering.png',image)
-	cv2.waitKey(0)
 	cv2.destroyAllWindows()
+"""
