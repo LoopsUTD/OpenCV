f82b5394ceabaa2917ae3a83d61854c208eb011e
David <dmmjr2@gmail.com>
2018-04-26 20:16:37 -0500

    Cleanup
    - Removed capture_img.py
    - Removed capture_raw.py
    - In cropper.py:
      - no longer importing lensFinder
      - removed "if main" code

--- cropper.py
+++ cropper.py
@@ -1,13 +1,12 @@
 import numpy as np
 import cv2
-import lensFinder	# only used when ran as main
 
 # Function cropImage()
 #  Given an image and a desirable circle,
 #  the function eliminates all image features
 #  outside the circle.
 # INPUTS:
-#  img	- 2-D array (3 channel) bitmap of image
+#  img	- 2-D array (1 or 3 channel) bitmap of image
 #  circle - 3-tuple, (x-center, y-center, radius)
 # OUTPUTS:
 #  newImg	- black image, with everything inside
@@ -22,16 +21,3 @@ def cropToCircle(img, circle):
 	newImg[:,:] = newImg[:,:] * img[:,:]
 #	newImg=newImg[(y-r):(y+r),(x-r):(x+r),:]
 	return newImg
-
-if __name__ == '__main__':
-	image = cv2.imread('withLens_DSC_0284.JPG')
-	circle = lensFinder.findLens(image.copy())
-	image = cv2.imread('DSC_0286.JPG')
-	image = cropToCircle(image,circle)
-	cv2.imshow('cropped',image)
-	cv2.waitKey(0)
-	cv2.imwrite('cropped.png',image)
-	circle2 = lensFinder.findLens(image)
-	print(circle)
-	print(circle2)
-	cv2.destroyAllWindows()
